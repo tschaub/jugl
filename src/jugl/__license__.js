5987de68e3591227b835e98f6ca0d542233fac69
tschaub <tim.schaub@gmail.com>
2010-04-02 10:54:44 -0400

    Full link to license.

--- src/jugl/__license__.js
+++ src/jugl/__license__.js
@@ -2,6 +2,6 @@
  * Jugl.js -- JavaScript Template Library
  *
  * Copyright 2007-2010 Tim Schaub
- * Released under the MIT license.  Please see license.txt for the full license.
+ * Released under the MIT license.  Please see 
+ * http://github.com/tschaub/jugl/blob/master/license.txt for the full license.
  */
-
